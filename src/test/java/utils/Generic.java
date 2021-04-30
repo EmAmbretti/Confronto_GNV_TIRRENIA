c4268d26274c11c2464a40337414092b80681d47
Giovannisorrentino <67473180+Giovannisorrentino@users.noreply.github.com>
2021-04-30 15:10:28 +0200

    Update ClickBy (Id e xPath) & SendKeysBy (Id E xPath)

--- src/test/java/utils/Generic.java
+++ src/test/java/utils/Generic.java
@@ -5,30 +5,27 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import model.CSVData;
-import model.WebData;
-import org.apache.commons.io.FileUtils;
-import org.joda.time.DateTime;
+
 import org.joda.time.LocalTime;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import model.WebData;
+
 public class Generic {
-	
+
 	public static void utente_apre_browser(WebDriver driver, String url, String nomeSito) throws Throwable {
 		driver = BeforeAndAfter.driver;
 		System.out.println("Opening URL: " + nomeSito);
 		driver.get(url);
 		driver.manage().window().maximize();
 		Thread.sleep(4000);
-		
 	}
-	
+
 	public static List<WebElement> findElementsByInnerText(WebElement parent, String rex) {
 		List<WebElement> childs = parent.findElements(By.cssSelector("*"));
 		List<WebElement> found = new ArrayList<>();
@@ -62,7 +59,7 @@ public class Generic {
 		}
 		return found;
 	}
-	
+
 	public static WebElement findElementByAttribute(List<WebElement> l, String attr, String rex) {
 		for (int i = 0; i < l.size(); ++i) {
 			WebElement entry = l.get(i);
@@ -72,106 +69,171 @@ public class Generic {
 		}
 		return null;
 	}
-	
-	public static void scrollPage(WebDriver driver, String scroll) throws Throwable{
-		JavascriptExecutor jse = (JavascriptExecutor)driver;
-		jse.executeScript("window.scrollBy(0,"+scroll+")");
+
+	public static void scrollPage(WebDriver driver, String scroll) throws Throwable {
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("window.scrollBy(0," + scroll + ")");
 		Thread.sleep(4000);
 	}
-	
-	public static Double confrontoPrezzi(WebDriver driver, Double importo1,String nomeSitoImp1,Double importo2,String nomeSitoImp2, Double importo3, String nomeSitoImp3) {
-		if(importo1==null) {
-			importo1=Double.MAX_VALUE;
+
+	public static Double confrontoPrezzi(WebDriver driver, Double importo1, String nomeSitoImp1, Double importo2,
+			String nomeSitoImp2, Double importo3, String nomeSitoImp3) {
+		if (importo1 == null) {
+			importo1 = Double.MAX_VALUE;
 		}
-		if(importo2==null) {
-			importo2=Double.MAX_VALUE;
+		if (importo2 == null) {
+			importo2 = Double.MAX_VALUE;
 		}
-		if(importo3==null) {
-			importo3=Double.MAX_VALUE;
+		if (importo3 == null) {
+			importo3 = Double.MAX_VALUE;
 		}
-		if(importo1 > importo2) {
-			if(importo2 > importo3) {
+		if (importo1 > importo2) {
+			if (importo2 > importo3) {
 				return importo3;
-			}else {
+			} else {
 				return importo2;
 			}
-		}else if(importo1 > importo3) {
+		} else if (importo1 > importo3) {
 			return importo3;
-		}else {
+		} else {
 			return importo1;
 		}
-		
-	}
-	
-	public static void clickByXPath(WebDriver driver, String xPath) throws Throwable {
-		driver.findElement(By.xpath(xPath)).click();
 
 	}
-	
+
+	public static void clickByXPath(WebDriver driver, String xPath) {
+		try {
+			WebElement element = driver.findElement(By.xpath(xPath));
+			if (element != null) {
+				element.click();
+				System.out.println("CLICKED: " + xPath);
+			}
+		} catch (Exception e) {
+			System.out.print("!ERRORE! clickByXPath: ");
+			System.out.print(xPath);
+			if (e instanceof org.openqa.selenium.NoSuchElementException) {
+				System.out.println(": Selenium.NoSuchElementException");
+			} else {
+				System.out.println(": " + e.getLocalizedMessage());
+			}
+		}
+	}
+
 	public static void clickById(WebDriver driver, String id) throws Throwable {
-		driver.findElement(By.id(id)).click();
+		try {
+			WebElement element = driver.findElement(By.xpath(id));
+			if (element != null) {
+				element.click();
+				System.out.println("CLICKED: " + id);
+			}
+		} catch (Exception e) {
+			System.out.print("!ERRORE! ClickById: ");
+			System.out.print(id);
+			if (e instanceof org.openqa.selenium.NoSuchElementException) {
+				System.out.println(": Selenium.NoSuchElementException");
+			} else {
+				System.out.println(": " + e.getLocalizedMessage());
+			}
+		}
 
 	}
-	
-	public static void sendKeysByXPath(WebDriver driver, String xPath, String keys) {
-		driver.findElement(By.xpath(xPath)).sendKeys(keys);
-		
+
+	public static void sendKeysByXPath(WebDriver driver, String xpath, String toSend) {
+		try {
+				WebElement element = driver.findElement(By.xpath(xpath));
+			if (element != null) {
+				element.sendKeys(toSend);
+				System.out.println("TESTO INSERITO, sendKeysByXPath: "+xpath);
+			}
+		} catch (Exception e) {
+			System.out.print("!ERRORE! SendKeysByXPath: ");
+			System.out.print(xpath);
+			if(e instanceof org.openqa.selenium.NoSuchElementException) {
+				System.out.println(": Selenium.NoSuchElementException");
+			} else {
+				System.out.println(": "+e.getLocalizedMessage());
+			}
+		}
 	}
-	
-	public static void sendKeysById(WebDriver driver, String id, String keys) {
-		driver.findElement(By.id(id)).sendKeys(keys);
-		
+
+	public static void sendKeysById(WebDriver driver, String id, String toSend) {
+		try {
+			WebElement element = driver.findElement(By.id(id));
+			if (element != null) {
+				element.sendKeys(toSend);
+				System.out.println("TESTO INSERITO, sendKeysById: "+id);
+			}
+		} catch (Exception e) {
+			System.out.print("!ERRORE! SendKeysById: ");
+			System.out.print(id);
+			if(e instanceof org.openqa.selenium.NoSuchElementException) {
+				System.out.println(": Selenium.NoSuchElementException");
+			} else {
+				System.out.println(": "+e.getLocalizedMessage());
+			}
+		}
 	}
-	
+
 	public static void clickByList(WebDriver driver, String xPath, int index) {
 		List<WebElement> elementList = driver.findElements(By.xpath(xPath));
 		elementList.get(index).click();
 	}
-	
-	public static void generaFileTxt(String tipologia, WebData sito1, WebData sito2, WebData sito3 ,Double prezzoTirrenia, Double prezzoGNV,Double prezzoGrimaldi,Double prezzoMigliore) {
-		String path=new File ("reportFiles\\").getAbsolutePath();
-		File itinerario = new File(path+"\\" + Path.CONFRONTO);
-		LocalDate data=LocalDate.now();
-		LocalTime time=LocalTime.now();
-		String timeStr=String.valueOf(time);
-		timeStr=timeStr.substring(0,5);
+
+	public static void generaFileTxt(String tipologia, WebData sito1, WebData sito2, WebData sito3,
+			Double prezzoTirrenia, Double prezzoGNV, Double prezzoGrimaldi, Double prezzoMigliore) {
+		String path = new File("reportFiles\\").getAbsolutePath();
+		File itinerario = new File(path + "\\" + Path.CONFRONTO);
+		LocalDate data = LocalDate.now();
+		LocalTime time = LocalTime.now();
+		String timeStr = String.valueOf(time);
+		timeStr = timeStr.substring(0, 5);
 		try {
-			FileWriter fw = new FileWriter(itinerario,true);
-			BufferedWriter bw= new BufferedWriter(fw);
-			bw.append("DATA: "+data+" ORE: "+timeStr+"\nCASO DI TEST: "+tipologia+"\nTRATTA: "+sito1.getTratta()+ "\nMESE: "+sito1.getMese()+"\nGIORNO: "+sito1.getGiorno()+"\nNUMERO PASSEGGERI ADULTI: "+sito1.getAdulti()+"\nNUMERO PASSEGGERI BAMBINI: "+sito1.getBambini()+"\nNUMERO PASSEGGERI ANIMALI: "+sito1.getAnimali()+"\nVEICOLO: "+sito1.getVeicolo());
-			if(sito1.getDisponibilita()==null) {
-				bw.append("\nPREZZO GNV: "+prezzoGNV);
-			}else {
-				bw.append("\nPREZZO GNV: Non è stato possibile rilasciare un preventivo per GNV per il seguente motivo: "+sito1.getDisponibilita());
+			FileWriter fw = new FileWriter(itinerario, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.append("DATA: " + data + " ORE: " + timeStr + "\nCASO DI TEST: " + tipologia + "\nTRATTA: "
+					+ sito1.getTratta() + "\nMESE: " + sito1.getMese() + "\nGIORNO: " + sito1.getGiorno()
+					+ "\nNUMERO PASSEGGERI ADULTI: " + sito1.getAdulti() + "\nNUMERO PASSEGGERI BAMBINI: "
+					+ sito1.getBambini() + "\nNUMERO PASSEGGERI ANIMALI: " + sito1.getAnimali() + "\nVEICOLO: "
+					+ sito1.getVeicolo());
+			if (sito1.getDisponibilita() == null) {
+				bw.append("\nPREZZO GNV: " + prezzoGNV);
+			} else {
+				bw.append(
+						"\nPREZZO GNV: Non è stato possibile rilasciare un preventivo per GNV per il seguente motivo: "
+								+ sito1.getDisponibilita());
 			}
-			if(sito2.getDisponibilita()==null) {
-				bw.append("\nPREZZO TIRRENIA: "+prezzoTirrenia);
-			}else {
-				bw.append("\nPREZZO TIRRENIA: Non è stato possibile rilasciare un preventivo per TIRRENIA per il seguente motivo: "+sito2.getDisponibilita());
+			if (sito2.getDisponibilita() == null) {
+				bw.append("\nPREZZO TIRRENIA: " + prezzoTirrenia);
+			} else {
+				bw.append(
+						"\nPREZZO TIRRENIA: Non è stato possibile rilasciare un preventivo per TIRRENIA per il seguente motivo: "
+								+ sito2.getDisponibilita());
 			}
-			if(sito3.getDisponibilita()==null) {
-				bw.append("\nPREZZO GRIMALDI: "+prezzoGrimaldi);
-			}else {
-				bw.append("\nPREZZO GRIMALDI: Non è stato possibile rilasciare un preventivo per GRIMALDI per il seguente motivo: "+sito3.getDisponibilita());
+			if (sito3.getDisponibilita() == null) {
+				bw.append("\nPREZZO GRIMALDI: " + prezzoGrimaldi);
+			} else {
+				bw.append(
+						"\nPREZZO GRIMALDI: Non è stato possibile rilasciare un preventivo per GRIMALDI per il seguente motivo: "
+								+ sito3.getDisponibilita());
 			}
-			if(prezzoMigliore!=Double.MAX_VALUE) {
-				bw.append("\nPREZZO MIGLIORE: "+prezzoMigliore+" DEL SITO: ");
-				if(prezzoMigliore==prezzoGNV) {
+			if (prezzoMigliore != Double.MAX_VALUE) {
+				bw.append("\nPREZZO MIGLIORE: " + prezzoMigliore + " DEL SITO: ");
+				if (prezzoMigliore == prezzoGNV) {
 					bw.append(sito1.getSito());
-				}else if(prezzoMigliore==prezzoTirrenia) {
+				} else if (prezzoMigliore == prezzoTirrenia) {
 					bw.append(sito2.getSito());
-				}else {
+				} else {
 					bw.append(sito3.getSito());
 				}
-			}else {
+			} else {
 				bw.append("\nNon è stato possibile rilasciare un preventivo.");
 			}
 			bw.append("\n\n-------------------------------------------------------------------\n\n");
 			bw.close();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
