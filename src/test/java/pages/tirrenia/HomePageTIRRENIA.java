130695bc60cad0e32d057fa881222cd11d352fb1
Mirko <mirko.terracciano@citelgroup.it>
2021-05-03 10:04:13 +0200

    Update 03/05/2021 10:04

--- src/test/java/pages/tirrenia/HomePageTIRRENIA.java
+++ src/test/java/pages/tirrenia/HomePageTIRRENIA.java
@@ -13,29 +13,30 @@ import utils.Generic;
 
 public class HomePageTIRRENIA {
 
-	public static void inserisciDatiTirrenia(WebDriver driver, EsitoSito esito, CSVData data) throws Throwable {
+
+	public static void inserisciDatiTirrenia(WebDriver driver, EsitoSito esito, CSVData sito) throws Throwable {
 		if(esito.getErrori() == null) {
 			///////
 			System.out.println("Inizio inserimento dati tirrenia");
 			//////
 			Thread.sleep(2000);
-			
+
 			//scorriCollegamento(driver, sito, esito);
 			checkboxTirrenia(driver, esito);
-			controlloCollegamentoTirrenia(driver, data, esito);
-			selezionaAndataTirrenia(driver, esito, data);
+			controlloCollegamentoTirrenia(driver, sito, esito);
+			selezionaAndataTirrenia(driver, esito, sito);
 			cliccaCalendario(driver, esito);
-			selezionaMeseTirrenia(driver, data, esito);
-			selezionaGiornoTirrenia(driver, data, esito);
-			
-			selezionaPasseggeriTirrenia(driver, data, esito);
-			
+			selezionaMeseTirrenia(driver, sito, esito);
+			selezionaGiornoTirrenia(driver, sito, esito);
 
+			selezionaPasseggeriTirrenia(driver, sito, esito);
+
+			gestioneVeicolo(driver, sito.getVeicolo());
 			Thread.sleep(2000);	
-			cliccaCercaTirrenia(driver, data, esito);
+			cliccaCercaTirrenia(driver, sito, esito);
 		}	
 	}
-	
+
 	private static void controlloCollegamentoTirrenia(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable {
 		for(int i = 1; i <= 4; i ++) {
 			WebElement element = driver.findElement(By.xpath("//*[@id=\"ContentPlaceHolder1_motore_ddl_destinazioni\"]/option[" + i + "]"));
@@ -50,26 +51,26 @@ public class HomePageTIRRENIA {
 		}
 		Thread.sleep(1000);
 	}
-	
+
 	private static void checkboxTirrenia(WebDriver driver, EsitoSito esito) throws Throwable {
 		if(esito.getErrori() == null) {
 			Generic.clickByXPath(driver, "//*[@id=\"ContentPlaceHolder1_motore_motore_verticale\"]/div/div[2]/div[1]/div[2]/div[1]/label[2]");
 			Thread.sleep(1000);
 		}
 	}
-	
+
 	private static void cliccaTratteTirrenia(WebDriver driver, EsitoSito esito) throws Throwable {
 		if(esito.getErrori() == null) {
 			Generic.clickById(driver, "tratte_andata");
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	private static void cliccaCollegamentoTirrenia(WebDriver driver) throws Throwable {
 		driver.findElement(By.id("ContentPlaceHolder1_motore_ddl_destinazioni"));
 		Thread.sleep(1000);
 	}
-	
+
 	private static void selezionaAndataTirrenia(WebDriver driver, EsitoSito esito, CSVData sito) throws Throwable{
 		if(esito.getErrori() == null) {
 			try {
@@ -80,7 +81,7 @@ public class HomePageTIRRENIA {
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	private static void cliccaCalendario(WebDriver driver, EsitoSito esito) throws Throwable {
 		if(esito.getErrori() == null) {
 			//Generic.clickById(driver, "arrival");
@@ -88,7 +89,7 @@ public class HomePageTIRRENIA {
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	private static void selezionaMeseTirrenia(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable{
 		if(esito.getErrori() == null) {
 			for(int i = 1; i <= 12; i ++) {
@@ -110,7 +111,7 @@ public class HomePageTIRRENIA {
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	private static void selezionaGiornoTirrenia(WebDriver driver,CSVData sito, EsitoSito esito) throws Throwable{
 		if(esito.getErrori()==null) {
 			boolean controllo = false;
@@ -135,11 +136,11 @@ public class HomePageTIRRENIA {
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	private static void selezionaAdultiTirrenia(WebDriver driver, String adulti) {
 		driver.findElement(By.xpath("//*[@id=\"select_NumeroAdultiTipo2Andata\"]/option["+(Integer.valueOf(adulti)+1)+"]")).click();
 	}
-	
+
 	private static void selezionaBambiniTirrenia(WebDriver driver,String bambini) throws Throwable{
 		driver.findElement(By.xpath("//*[@id=\"select_NumeroPasseggeriAndata\"]/option["+(Integer.valueOf(bambini)+1)+"]")).click();
 		for(int i=0;i<Integer.valueOf(bambini);i++) {
@@ -147,18 +148,47 @@ public class HomePageTIRRENIA {
 			driver.findElement(By.id("eta_"+i+"_Andata")).sendKeys("10");
 		}
 	}
-	
+
 	private static void selezionaAnimaliTirrenia(WebDriver driver, String animali) {
 		driver.findElement(By.xpath("//*[@id=\"select_NumeroAnimaliAndata\"]/option["+(Integer.valueOf(animali)+1)+"]")).click();
 	}
-	
-		
+
+	private static void gestioneVeicolo(WebDriver driver, String veicolo)throws Throwable{
+
+		Generic.clickByXPath(driver, "//*[@id=\"input_VeicoliAndata\"]");
+		if(veicolo.equalsIgnoreCase("No")) {
+			Generic.clickByXPath(driver, "//a[1]/div/label");
+		}else if(veicolo.equalsIgnoreCase("Car")||veicolo.equalsIgnoreCase("Car5m")) {
+			Generic.clickByXPath(driver, "//a[2]/div/label");
+			Generic.clickByXPath(driver, "//*[@id=\"select_MarcaVeicoloAndata\"]");
+			Thread.sleep(1000);
+			if(veicolo.equalsIgnoreCase("Car")){
+				Generic.clickByXPath(driver, "//*[@id=\"select_MarcaVeicoloAndata\"]/option[contains(text(),'fiat')]");
+				Generic.clickById(driver, "select_ModelloVeicoloAndata");
+				Generic.clickByXPath(driver, "//*[@id=\"select_ModelloVeicoloAndata\"]/option[contains(text(),'panda (2013-)')]");
+			}else if(veicolo.equalsIgnoreCase("Car5m")) {
+				Generic.clickByXPath(driver, "//*[@id=\"select_MarcaVeicoloAndata\"]/option[contains(text(),'audi')]");
+				Generic.clickById(driver, "select_ModelloVeicoloAndata");
+				Generic.clickByXPath(driver, "//*[@id=\"select_ModelloVeicoloAndata\"]/option[contains(text(),'q7 (2018-)')]");
+			}
+		}else if(veicolo.equalsIgnoreCase("Moto")){
+			Generic.clickByXPath(driver, "//a[6]/div/label");
+			Generic.clickById(driver, "select_QuantitaVeicoloAndata");
+			Generic.clickByXPath(driver, "//*[@id=\"select_QuantitaVeicoloAndata\"]/option[2]");
+		}else if(veicolo.equalsIgnoreCase("CMP")) {
+			Generic.clickByXPath(driver, "//a[3]/div/label");
+			driver.findElement(By.id("input_LunghezzaVeicoloAndata")).sendKeys("750");
+			driver.findElement(By.id("input_AltezzaVeicoloAndata")).sendKeys("350");
+		}
+		Generic.clickByXPath(driver, "//*[@id=\"ContentPlaceHolder1_motore_motore_verticale\"]/div/div[2]/div[4]/div/button");
+	}
+
 	public static void cliccaCercaTirrenia(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable {
 		if (esito.getErrori() == null) {
 			Generic.clickByXPath(driver, "//*[@id=\"ContentPlaceHolder1_motore_Button_Cerca\"]");
 		}	
 	}
-	
+
 	private static void scorriCollegamento(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable {
 		List<WebElement> listaCollegamento = driver.findElements(By.xpath("//*[@id=\"ContentPlaceHolder1_motore_ddl_destinazioni\"]/option"));
 		cliccaCollegamentoTirrenia(driver);
@@ -175,14 +205,14 @@ public class HomePageTIRRENIA {
 			Thread.sleep(500);	
 		}
 	}
-	
+
 	private static void cliccaTratteTirrenia(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable {
 		if(esito.getErrori() == null) {
 			Generic.clickById(driver, "tratte_andata");
 			Thread.sleep(2000);
 		}
 	}
-	
+
 	public static void selezionaPasseggeriTirrenia(WebDriver driver, CSVData sito, EsitoSito esito) throws Throwable {
 		if(esito.getErrori() == null) {
 			Generic.clickByXPath(driver, "//*[@id=\"input_NumeroPaxAndata\"]");
@@ -198,10 +228,9 @@ public class HomePageTIRRENIA {
 			Thread.sleep(2000);
 			Generic.clickByXPath(driver, "//*[@id=\"ContentPlaceHolder1_motore_motore_verticale\"]/div/div[2]/div[2]/div[3]/button");
 			Thread.sleep(2000);
-		}
-		
+		}	
 	}
-	
+
 	public static void bypassFrame(WebDriver driver) throws Throwable {
 		driver.switchTo().frame("tlines").findElement(By.id("ContentPlaceHolder1_motore_ddl_destinazioni")).click();
 		Thread.sleep(3000);
--- src/test/java/steps/Tirrenia.java
+++ src/test/java/steps/Tirrenia.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 
 import model.CSVData;
 import model.EsitoSito;
-import pages.HomePage;
 import pages.Recap;
 import pages.tirrenia.HomePageTIRRENIA;
 import pages.tirrenia.RecapPageTIRRENIA;
@@ -16,7 +15,7 @@ public class Tirrenia {
 	public static void stepTirrenia(WebDriver driver, CSVData testData) throws Throwable {
 		EsitoSito sitoTIRRENIA = new EsitoSito("TIRRENIA",testData);
 		Generic.utente_apre_browser(driver, "https://www.tirrenia-prenotazioni.it", sitoTIRRENIA.getSito());
-		HomePage.bypassFrame(driver);
+		HomePageTIRRENIA.bypassFrame(driver);
 		HomePageTIRRENIA.inserisciDatiTirrenia(driver, sitoTIRRENIA, testData);
 		Recap.switchPage(driver,sitoTIRRENIA);
 		RecapPageTIRRENIA.controlloDisponibilitaFinaleTirrenia(driver, sitoTIRRENIA);
