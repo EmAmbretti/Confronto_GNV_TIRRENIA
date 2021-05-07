8a2e76e0343d70c3619a3f29eb8e01d44e787f9b
Giovannisorrentino <67473180+Giovannisorrentino@users.noreply.github.com>
2021-05-07 13:14:45 +0200

    Update GNV

--- src/test/java/steps/GNV.java
+++ src/test/java/steps/GNV.java
@@ -10,11 +10,12 @@ import utils.BeforeAndAfter;
 
 public class GNV {
 	
-	public static void allMethods(CSVData data) throws Throwable {
+	public static EsitoSito allMethods(CSVData data) throws Throwable {
 		WebDriver driver = BeforeAndAfter.before("GNV");
 		EsitoSito esito = new EsitoSito("GNV", data);
 		HomePageGNV.selezionaItinerarioGNV(driver, esito);
 		RecapPageGNV.recuperoImportoGNV(driver, esito);
+		return esito;
 	}
 
 }
