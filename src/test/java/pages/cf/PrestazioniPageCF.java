d0697c1ed1bce9170de01df779f47340b0ed316e
Giovannisorrentino <67473180+Giovannisorrentino@users.noreply.github.com>
2021-05-10 10:12:01 +0200

    Update PrestazioniPageCF.java

--- src/test/java/pages/cf/PrestazioniPageCF.java
+++ src/test/java/pages/cf/PrestazioniPageCF.java
@@ -3,7 +3,6 @@ package pages.cf;
 import java.util.ArrayList;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
