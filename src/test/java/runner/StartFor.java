a588980ab5c6e845298afcce5ed65c154ab2f8c5
Giovannisorrentino <67473180+Giovannisorrentino@users.noreply.github.com>
2021-05-12 10:32:46 +0200

    Update Start

--- src/test/java/runner/StartFor.java
+++ src/test/java/runner/StartFor.java
@@ -21,29 +21,7 @@ import utils.Path;
 public class StartFor {
 
 	public static void main(String[] args) {
-		//CSVData testData = null;
-		/*
-		 * testData=CSVExtractor.getTestDataById("TC11", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC12", Path.PATH);
-		 * CorsicaFerries.automation(testData);
 
-		testData = CSVExtractor.getTestDataById("TC13", Path.PATH);
-		CorsicaFerries.automation(testData);*/
-		/*
-		 * testData=CSVExtractor.getTestDataById("TC14", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC15", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC16", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC17", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC18", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 * testData=CSVExtractor.getTestDataById("TC19", Path.PATH);
-		 * CorsicaFerries.automation(testData);
-		 */
 		TreeMap<LocalDate, ArrayList<Differenza>> mappaConfronti = new TreeMap<LocalDate, ArrayList<Differenza>>();
 
 		ArrayList<CSVData> datiCSV = CSVExtractor.process(Path.PATH);
@@ -59,65 +37,60 @@ public class StartFor {
 		int numeroDiGiorniDaLanciare = Generic.controlloStagione(Config.get("stagione"));
 
 		for (int i = 0; i <= numeroDiGiorniDaLanciare; i++) {
-			System.out.println("i: "+i);
+			try {
+				ArrayList<Differenza> listaDifferenze = new ArrayList<Differenza>();
 
-			System.out.println(dataPrenotazione);
-
-			ArrayList<Differenza> listaDifferenze = new ArrayList<Differenza>();
-
-			int prezzoDaEliminare = 10;
-			for (int x = 0; x < datiCSV.size(); x++) {
-				System.out.println("X: "+x+", DATA PRENOTAZIONE: "+dataPrenotazione);
-				Generic.setDataPrenotazioneNelModelCSV(datiCSV.get(x), dataPrenotazione);
-
-				///////////////////test con step
-
-				EsitoSito esitoGrimaldi = null;
-				try {
-					esitoGrimaldi = GrimaldiLines.stepGrimaldi(datiCSV.get(x));
-				} catch (Throwable e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				EsitoSito esitoCompetitor= null;
-				if(Config.get("competitor").equalsIgnoreCase("GNV")) {
+				for (int x = 0; x < datiCSV.size(); x++) {
 					try {
-						esitoCompetitor = GNV.allMethods(datiCSV.get(x));
-					}catch(Throwable e) {
+						Generic.setDataPrenotazioneNelModelCSV(datiCSV.get(x), dataPrenotazione);
+
+						EsitoSito esitoGrimaldi = null;
+						try {
+							esitoGrimaldi = GrimaldiLines.stepGrimaldi(datiCSV.get(x));
+						} catch (Throwable e) {
+							e.printStackTrace();
+						}
+						EsitoSito esitoCompetitor = null;
+						if (Config.get("competitor").equalsIgnoreCase("GNV")) {
+							try {
+								esitoCompetitor = GNV.allMethods(datiCSV.get(x));
+							} catch (Throwable e) {
+								e.printStackTrace();
+							}
+						} else if (Config.get("competitor").equalsIgnoreCase("TIRRENIA")) {
+							try {
+								esitoCompetitor = Tirrenia.stepTirrenia(datiCSV.get(x));
+							} catch (Throwable e) {
+								e.printStackTrace();
+							}
+						} else if (Config.get("competitor").equalsIgnoreCase("MOBY")) {
+							esitoCompetitor = Moby.allMethods(datiCSV.get(x));
+						} else if (Config.get("competitor").equalsIgnoreCase("CF")
+								|| Config.get("competitor").equalsIgnoreCase("CORSICA FERRIES")) {
+							esitoCompetitor = CorsicaFerries.automation(datiCSV.get(x));
+						}
+
+						Differenza diff = new Differenza(esitoGrimaldi, esitoCompetitor);
+						listaDifferenze.add(diff);
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
-				} else if(Config.get("competitor").equalsIgnoreCase("TIRRENIA")) {
-					try {
-						esitoCompetitor = Tirrenia.stepTirrenia(datiCSV.get(x));
-					} catch (Throwable e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} else if(Config.get("competitor").equalsIgnoreCase("MOBY")) {
-					esitoCompetitor = Moby.allMethods(datiCSV.get(x));
-				} else if(Config.get("competitor").equalsIgnoreCase("CF") || Config.get("competitor").equalsIgnoreCase("CORSICA FERRIES")){
-					esitoCompetitor = CorsicaFerries.automation(datiCSV.get(x));
 				}
 
-				Differenza diff = new Differenza(esitoGrimaldi, esitoCompetitor);
-				listaDifferenze.add(diff);
+				if (!listaDifferenze.isEmpty()) {
+					mappaConfronti.put(dataPrenotazione, listaDifferenze);
+				} else {
+					System.out.println("LISTA DIFFERENZE VUOTA");
+				}
 
+				if (!dataPrenotazione.equals(dataFine)) {
+					dataPrenotazione = dataPrenotazione.plusDays(1);
+				} else {
+					break;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-
-			if(!listaDifferenze.isEmpty()) {
-				mappaConfronti.put(dataPrenotazione, listaDifferenze);
-			} else {
-				System.out.println("LISTA DIFFERENZE VUOTA");
-			}
-
-			if (!dataPrenotazione.equals(dataFine)) {
-				System.out.println("AUMENTO LA DATA");
-				dataPrenotazione = dataPrenotazione.plusDays(1);
-			} else {
-				break;
-			}
-
-
 		}
 		ExcelMaker.createReport(mappaConfronti, Config.get("path_cartella_di_destinazione"));
 	}
--- src/test/java/runner/StartSingleStep.java
+++ src/test/java/runner/StartSingleStep.java
@@ -3,6 +3,7 @@ package runner;
 import java.time.LocalDate;
 
 import model.CSVData;
+import steps.CorsicaFerries;
 import steps.Tirrenia;
 import utils.CSVExtractor;
 import utils.Generic;
@@ -11,6 +12,13 @@ import utils.Path;
 public class StartSingleStep {
 
 	public static void main(String[] args) {
+		/*
+		 * testData=CSVExtractor.getTestDataById("TC11", Path.PATH);
+		 * CorsicaFerries.automation(testData);
+		 * testData=CSVExtractor.getTestDataById("TC12", Path.PATH);
+		 * CorsicaFerries.automation(testData);
+		 */
+		
 		LocalDate dataInizio = null;
 		LocalDate dataFine = null;
 		
@@ -18,14 +26,100 @@ public class StartSingleStep {
 		dataFine = Generic.setDataFine("ALTA");
 
 		LocalDate dataPrenotazione = dataInizio;
-		CSVData testData = CSVExtractor.getTestDataById("TestCase1", Path.PATH);
+		CSVData testData = null;
+		
+		testData = CSVExtractor.getTestDataById("TC9", Path.PATH);
 		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
-		try {
-			Tirrenia.stepTirrenia(testData);
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		CorsicaFerries.automation(testData);
+		
+		testData = CSVExtractor.getTestDataById("TC10", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData = CSVExtractor.getTestDataById("TC11", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData = CSVExtractor.getTestDataById("TC12", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData = CSVExtractor.getTestDataById("TC13", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC14", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC15", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC16", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC17", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC18", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		
+		testData=CSVExtractor.getTestDataById("TC19", Path.PATH);
+		Generic.setDataPrenotazioneNelModelCSV(testData, dataPrenotazione);
+		CorsicaFerries.automation(testData);
+		/*
+		
+		TreeMap<LocalDate, ArrayList<Differenza>> mappaConfronti = new TreeMap<LocalDate, ArrayList<Differenza>>();
+		
+		ArrayList<CSVData> datiCSV = CSVExtractor.process(Path.PATH);
+		
+		LocalDate dataInizio = null;
+		LocalDate dataFine = null;
+		
+		dataInizio = Generic.setDataInizio("ALTA");
+		dataFine = Generic.setDataFine("ALTA");
+
+		LocalDate dataPrenotazione = dataInizio;
+		for (int i = 0; i < datiCSV.size(); i++) {
+			
+			int numeroDiGiorniDaLanciare = Generic.controlloStagione(Config.get("stagione"));
+			
+			ArrayList<Differenza> listaDifferenze = new ArrayList<Differenza>();
+			
+			int prezzoDaEliminare = 10;
+			
+			for (int x = 0; x <= numeroDiGiorniDaLanciare; x++) {
+				System.out.println("X: "+x+", DATA PRENOTAZIONE: "+dataPrenotazione);
+				Generic.setDataPrenotazioneNelModelCSV(datiCSV.get(i), dataPrenotazione);
+				
+				EsitoSito esitoGrimaldi = new EsitoSito("grimaldi", datiCSV.get(i));
+				EsitoSito esitoTirrenia = new EsitoSito("tirrenia", datiCSV.get(i));
+
+				esitoGrimaldi.setPrezzo(String.valueOf(100 + (prezzoDaEliminare * (x+1))));
+				esitoTirrenia.setPrezzo(String.valueOf(120 + (prezzoDaEliminare * (x+1))));
+				
+				Differenza diff = new Differenza(esitoGrimaldi, esitoTirrenia);
+				listaDifferenze.add(diff);
+			}
+
+			if (!dataPrenotazione.equals(dataFine)) {
+				System.out.println("AUMENTO LA DATAAAAAAAAAAAAAAAAAA");
+				dataPrenotazione = dataPrenotazione.plusDays(1);
+			} else {
+				break;
+			}
+
+			if(!listaDifferenze.isEmpty()) {
+				mappaConfronti.put(dataPrenotazione, listaDifferenze);
+			} else {
+				System.out.println("LISTA DIFFERENZE VUOTA");
+			}
 		}
+		ExcelMaker.createReport(mappaConfronti, "C:\\Users\\giovanni.sorrentino\\Desktop");*/
 	}
 
 }
