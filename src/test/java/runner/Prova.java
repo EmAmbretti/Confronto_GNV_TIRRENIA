bc6c734a2afc653c5c0954ea31215a55589176fd
Mirko <mirko.terracciano@citelgroup.it>
2021-05-12 12:10:44 +0200

    Create Prova.java

---
+++ src/test/java/runner/Prova.java
@@ -0,0 +1,15 @@
+package runner;
+
+import utils.Generic;
+
+public class Prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String frase= "C\blabla\bla\blabla.";
+		String frasePathata = Generic.toPath(frase);
+		System.out.println(frase);
+		System.out.println(frasePathata);
+	}
+
+}
